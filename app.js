231c47c3a8ad5bab6cd1cc510ba844ee32d5b1fb
webQbe <webcubesolutionslk@gmail.com>
2024-10-30 18:35:36 +0530

    Added 100 squares to grid with createBoard()

--- app.js
+++ app.js
@@ -3,10 +3,38 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Define DOM Elements
-    const grid = document.querySelector('grid');
+    const grid = document.querySelector('.grid');
 
     // Set Initial Grid Width
     // Grid width = 10 Squares
     let width = 10;
+
+    // Intialize Squares Array
+    let squares = [];
+
+    // Create Board
+    function createBoard(){
+
+        // Iterate until 100 squares ( width * width ) are created 
+        for(let i = 0; i < width*width; i++){
+            
+            // Create A Square
+            const square = document.createElement('div');
+
+            // Set Current Iteration Number as Current Square div ID
+            square.setAttribute('id', i);
+
+            // Add Current Square to Grid
+            grid.appendChild(square);
+
+            // Add Current Square to squares Array
+            squares.push(square);
+
+        }
+
+    }
+
+    // Create 100 square divs
+    createBoard();
 
 });
