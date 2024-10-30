cf687cded9c0da7a6e4da8af3403afebdaed0216
webQbe <webcubesolutionslk@gmail.com>
2024-10-30 18:13:37 +0530

    Defined DOM elements & set grid width to 10 squares

---
+++ app.js
@@ -0,0 +1,12 @@
+// Add Event Listener
+// To make sure all HTML is loaded before running JS code
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Define DOM Elements
+    const grid = document.querySelector('grid');
+
+    // Set Initial Grid Width
+    // Grid width = 10 Squares
+    let width = 10;
+
+});
