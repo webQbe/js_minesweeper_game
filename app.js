482cf23909fb36f6e3f6558e0b2f0abc90183fd3
webQbe <webcubesolutionslk@gmail.com>
2024-10-30 21:32:53 +0530

    Count if South square has a Bomb when current Square is between 0 - 88

--- app.js
+++ app.js
@@ -130,12 +130,18 @@ document.addEventListener('DOMContentLoaded', () => {
                     // South West Square has A Bomb
                 if(i < 90 && !isLeftEdge && squares[i - 1 + width].classList.contains('bomb')) totalBombs++
 
-                
+
                 // Add to total If
                     // Current Square is between 0 - 87 &
                     // Current Square is NOT at Right Edge &
                     // South Square has A Bomb
                 if(i < 88 && !isRightEdge && squares[i + 1 + width].classList.contains('bomb')) totalBombs++
+
+                
+                // Add to total If
+                    // Current Square is between 0 - 88 &
+                    // South Square has A Bomb
+                if(i < 89 && squares[i + width].classList.contains('bomb')) totalBombs++
 
 
                 // Add totalBombs to Current Square Data Attr
